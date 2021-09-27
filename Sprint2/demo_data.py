68c27d20a34afee005dde0e7e5916dd1d5ee5c9a
artifice_shell <58102418+Artifice-Shell@users.noreply.github.com>
2021-09-27 09:33:20 -0400

    Commit_final_test
    Attempting to get all fines in the right location.

---
+++ Sprint2/demo_data.py
@@ -0,0 +1,26 @@
+import sqlite3
+CONN = sqlite3.connect('demo_data.sqlite3')
+DATA = [
+  ('g', 3, 9),
+  ('v', 5, 7),
+  ('f', 8, 7)
+]
+
+def make_db():
+  curs = CONN.cursor()
+  curs.execute('CREATE TABLE demo (s char(1), x int, y int);')
+  for datum in DATA:
+    curs.execute('INSERT INTO demo (s,x,y) VALUES' + str(datum))
+  curs.close()
+  CONN.commit()
+
+def run_queries():
+  """Run and print output from queries for sprint challenge questions."""
+  curs = CONN.cursor()
+  print(curs.execute('SELECT COUNT(*) FROM demo;').fetchall())
+  print(curs.execute('SELECT COUNT(*) FROM demo WHERE x >= 5 AND y >=5;').fetchall())
+  print(curs.execute('SELECT COUNT(DISTINCT y) FROM demo;').fetchall())
+
+if __name__ == "__main__":
+  make_db()
+  run_queries()
